e49ac47043da259e7621b2071640911149f39ccc
스냅팀/심정우 <big.sim@musinsa.com>
2022-08-15 13:45:50 +0900

    21강. 책의 분야 추가하기

--- library-app-kotlin/src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ library-app-kotlin/src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -35,7 +35,7 @@ class BookServiceTest @Autowired constructor(
     @DisplayName("책 등록이 정상 동작함")
     fun saveBookTest() {
         //given
-        val request = BookRequest("이상한 나라의 엘리스")
+        val request = BookRequest("이상한 나라의 엘리스", "COMPUTER")
 
         //when
         bookService.saveBook(request)
@@ -44,13 +44,16 @@ class BookServiceTest @Autowired constructor(
         val books = bookRepository.findAll()
         assertThat(books).hasSize(1)
         assertThat(books[0].name).isEqualTo("이상한 나라의 엘리스")
+        assertThat(books[0].type).isEqualTo("COMPUTER")
     }
 
     @Test
     @DisplayName("책 대출이 정상 동작함")
     fun loanBookTest() {
         //given
-        bookRepository.save(Book("이상한 나라의 엘리스"))
+        bookRepository.save(Book.fixture("이상한 나라의 엘리스"))
+
+
         val savedUser = userRepository.save(
             User(
                 "big",
@@ -74,7 +77,7 @@ class BookServiceTest @Autowired constructor(
     @DisplayName("책 진작 대출되어 있다면, 신규 대출이 실패함")
     fun loanBookFailTest() {
         //given
-        bookRepository.save(Book("이상한 나라의 엘리스"))
+        bookRepository.save(Book.fixture("이상한 나라의 엘리스"))
         val savedUser = userRepository.save(
             User(
                 "big",
